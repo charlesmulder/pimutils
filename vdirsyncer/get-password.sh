b091b8ec1b7148528796b81567cc61f3a24eb40f
Charles Mulder <7zh6dj9f9@mozmail.com>
2022-01-13 16:18:09 +0000

    self hosted baikal

--- vdirsyncer/get-password.sh
+++ vdirsyncer/get-password.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-secret-tool lookup name keepassxc | keepassxc-cli show -qa Password ~/Dropbox/Mulder/Public/Charles/charlesmulder.kdbx /Passwords/charliemulder@protonmail.com/tab.digital
+#secret-tool lookup name keepassxc | keepassxc-cli show -qa Password ~/Dropbox/Mulder/Public/Charles/charlesmulder.kdbx /Passwords/charliemulder@protonmail.com/tab.digital
+secret-tool lookup name keepassxc | keepassxc-cli show -qa Password ~/Dropbox/Mulder/Public/Charles/charlesmulder.kdbx /Passwords/charlesmulder@tutamail.com/dav.charlesmulder.net/dav.php/
